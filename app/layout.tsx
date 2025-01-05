7ba7887a322abe07ceb4afe112364640a00ad436
exy <teslawiktor@gmail.com>
2025-01-05 14:12:43 +0100

    insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 //components
 import Header from "@/components/Header";
