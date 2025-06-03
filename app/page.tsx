93299ede688b40414ace9898dfc6223a0e379c5d
exy <teslawiktor@gmail.com>
2025-06-03 19:48:03 +0200

    tak

--- app/page.tsx
+++ app/page.tsx
@@ -206,7 +206,7 @@ export default function Smaki() {
         opacity: 1,
         transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
       }}
-      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0 xl:mt-[-50]"
     >
       <div className="container mx-auto">
         <Tabs
